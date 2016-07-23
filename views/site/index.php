9f111f9f8c4b113686b826bae28a124013c7eb3a
Igor Leahu <igor.leahu24@gmail.com>
2016-07-23 08:52:59 +0300

    init

---
+++ views/site/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'City';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+
+        <h1>City</h1>
+
+        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias consectetur cum dolorem eos et
+            exercitationem explicabo facere ipsum laboriosam libero necessitatibus, neque odio, officia quo repudiandae
+            sequi sunt? Tenetur, voluptate.</p>
+
+    </div>
+</div>
